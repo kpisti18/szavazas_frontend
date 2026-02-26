5d58928176e8adace1bb0bdeadc450dd7519eec5
kpisti18 <k.istvanzoltan@gmail.com>
2026-02-26 13:23:24 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 
-
+// Hello
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
