4ad08a425bf34e9708c6282dcb3dc5ea794978f3
kpisti18 <k.istvanzoltan@gmail.com>
2026-02-26 13:36:15 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />}/>
         <Route path='/login' element={<Login />}/>
+        <Route path='/login' element={<Login />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
