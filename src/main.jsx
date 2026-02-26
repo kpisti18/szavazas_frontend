760970495e170966613dd852a6b3bd2df487eb95
kpisti18 <k.istvanzoltan@gmail.com>
2026-02-26 13:26:02 +0100

    asdasd

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 
-// Hello
+// Hello2
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
